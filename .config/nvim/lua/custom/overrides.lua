8c3788733191805173d3682b191db06c44fb08d5
Emmanuel Chee-zaram Okeke <cheezaram.okeke@gmail.com>
2024-03-02 21:38:03 +0100

    specify path to codescreenshot directory

--- .config/nvim/lua/custom/overrides.lua
+++ .config/nvim/lua/custom/overrides.lua
@@ -167,6 +167,10 @@ M.nvim_silicon = {
 	font = "JetBrainsMono Nerd Font=34;Noto Color Emoji=34",
 	theme = "Dracula",
 	background = "#94e2d5",
+	-- to_clipboard = true,
+	output = function()
+		return "~/Pictures/CodeScreenshots/" .. os.date("!%Y-%m-%dT%H-%M-%S") .. "_code.png"
+	end,
 	window_title = function()
 		return vim.fn.fnamemodify(vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf()), ":t")
 	end,
