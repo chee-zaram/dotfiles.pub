7ef83d4204776925d8a129e21b37a84703d80f4e
Emmanuel Chee-zaram Okeke <cheezaram.okeke@gmail.com>
2024-03-05 11:32:01 +0100

    add python nvim-dap config

--- .config/nvim/lua/custom/configs/null-ls.lua
+++ .config/nvim/lua/custom/configs/null-ls.lua
@@ -1,14 +1,16 @@
-local null_ls = require("null-ls")
+local ok, null_ls = pcall(require, "null-ls")
+local Opts = {}
+if not ok then
+	return Opts
+end
 
-local opts = {
-	sources = {
-		null_ls.builtins.diagnostics.mypy.with({
-			extra_args = function()
-				local virtual = os.getenv("VIRTUAL_ENV") or os.getenv("CONDA_PREFIX") or "/usr"
-				return { "--python-executable", virtual .. "/bin/python3" }
-			end,
-		}),
-	},
+Opts.sources = {
+	null_ls.builtins.diagnostics.mypy.with({
+		extra_args = function()
+			local virtual = os.getenv("VIRTUAL_ENV") or os.getenv("CONDA_PREFIX") or "/usr"
+			return { "--python-executable", virtual .. "/bin/python3" }
+		end,
+	}),
 }
 
-return opts
+return Opts
