08456718f528ef70ec35d8aa9e6892460065ed83
Emmanuel Chee-zaram Okeke <cheezaram.okeke@gmail.com>
2024-03-12 23:38:23 +0100

    fix descriptions for mappings to show up well on nvchad cheatsheet

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -3,19 +3,18 @@ require "nvchad.mappings"
 local map = vim.keymap.set
 local opts = { silent = true }
 
--- Manipulatin buffer
+-- Manipulating buffer
 map(
   "n",
   "<leader>w",
   ":w<Return>",
-  vim.tbl_extend("keep", { desc = "Save current buffer" }, opts)
+  vim.tbl_extend("keep", { desc = "Base Write current buffer" }, opts)
 )
-
 map(
   "n",
   "<leader>wo",
   ":wq<Return>",
-  vim.tbl_extend("keep", { desc = "Save current buffer and quit" }, opts)
+  vim.tbl_extend("keep", { desc = "Base Write current buffer and quit" }, opts)
 )
 map(
   "n",
@@ -23,7 +22,7 @@ map(
   ":q!<Return>",
   vim.tbl_extend(
     "keep",
-    { desc = "Quit the current buffer without saving" },
+    { desc = "Base Close the current buffer without saving" },
     opts
   )
 )
@@ -31,33 +30,31 @@ map(
   "n",
   "<leader>qo",
   ":qa!<Return>",
-  vim.tbl_extend("keep", { desc = "Quit all buffers without saving" }, opts)
+  vim.tbl_extend("keep", { desc = "Base All buffers without saving" }, opts)
 )
 map(
   "n",
   "<leader>wq",
   ":wqa<Return>",
-  vim.tbl_extend("keep", { desc = "Save and quit all buffers" }, opts)
+  vim.tbl_extend("keep", { desc = "Base Write and quit all buffers" }, opts)
 )
-
--- Deleting
 map(
   "n",
   "x",
   '"_x',
-  vim.tbl_extend("keep", { desc = "Delete into blackhole register" }, opts)
+  vim.tbl_extend("keep", { desc = "Base Delete character(s) under cursor to blackhole register" }, opts)
 )
 map(
   "n",
   "+",
   "<C-a>",
-  vim.tbl_extend("keep", { desc = "Increment counter" }, opts)
+  vim.tbl_extend("keep", { desc = "Base Increment counter" }, opts)
 )
 map(
   "n",
   "-",
   "<C-x>",
-  vim.tbl_extend("keep", { desc = "Decrement counter" }, opts)
+  vim.tbl_extend("keep", { desc = "Base Decrement counter" }, opts)
 )
 map(
   "n",
@@ -65,39 +62,53 @@ map(
   'vb"_d',
   vim.tbl_extend(
     "keep",
-    { desc = "Delete a word backwards into blackhole register" },
+    { desc = "Base Delete a word backwards into blackhole register" },
     opts
   )
 )
-
--- Tab maps
+map(
+  { "i", "v" },
+  "jk",
+  "<ESC>",
+  vim.tbl_extend("keep", { desc = "Base Exit current mode" }, opts)
+)
+map(
+  "i",
+  "<C-o>",
+  "<esc>viwUea",
+  vim.tbl_extend("keep", { desc = "Base Change word to uppercase" }, opts)
+)
+map(
+  "t",
+  "<leader>jk",
+  "<C-\\><C-n>",
+  vim.tbl_extend("keep", { desc = "Base Exit terminal mode" }, opts)
+)
 map(
   "n",
   "<leader>te",
   ":tabedit<Return>",
-  vim.tbl_extend("keep", { desc = "Open new tab" }, opts)
+  vim.tbl_extend("keep", { desc = "Base Open new tab" }, opts)
 )
 map(
   "n",
   "<leader>tn",
   ":tabn<Return>",
-  vim.tbl_extend("keep", { desc = "Move to next tab" }, opts)
+  vim.tbl_extend("keep", { desc = "Base Move to next tab" }, opts)
 )
 map(
   "n",
   "<leader>tp",
   ":tabp<Return>",
-  vim.tbl_extend("keep", { desc = "Move to previous tab" }, opts)
+  vim.tbl_extend("keep", { desc = "Base Move to previous tab" }, opts)
 )
-
--- Split maps
 map(
   "n",
   "<leader>sh",
   ":split<Return><C-w>w",
   vim.tbl_extend(
     "keep",
-    { desc = "Horizontal split and move to new split" },
+    { desc = "Base Horizontal split and move to new split" },
     opts
   )
 )
@@ -107,59 +118,54 @@ map(
   ":vsplit<Return><C-w>w",
   vim.tbl_extend(
     "keep",
-    { desc = "Vertical split and move to new split" },
+    { desc = "Base Vertical split and move to new split" },
     opts
   )
 )
 map(
   "n",
-  "<leader>sa",
-  "gg<S-v>G",
-  vim.tbl_extend("keep", { desc = "Select all in file" }, opts)
-)
-map(
-  "n",
   "<leader>=",
   "<C-w>=",
-  vim.tbl_extend("keep", { desc = "Resize all splits" }, opts)
+  vim.tbl_extend("keep", { desc = "Base Resize all splits" }, opts)
 )
 map(
   "n",
   "<leader><Space>",
   "<C-w>w",
-  vim.tbl_extend("keep", { desc = "Move to next split" }, opts)
+  vim.tbl_extend("keep", { desc = "Base Move to next split" }, opts)
 )
-
--- Terminal maps
 map(
   "n",
   "<leader>st",
   ":vsplit<Return><C-w>w:terminal<Return>",
   vim.tbl_extend(
     "keep",
-    { desc = "Start terminal in new vertical split" },
+    { desc = "Base Start terminal in new vertical split" },
     opts
   )
 )
+map("n", "<leader>tt", function()
+  require("base46").toggle_transparency()
+end, vim.tbl_extend("keep", { desc = "Base Toggle Transparency" }, opts))
 
 -- Fugitive
 map(
   "n",
   "<leader>gw",
   ":Gwrite<Return>",
-  vim.tbl_extend("keep", { desc = "Save current buffer and git add" }, opts)
+  vim.tbl_extend("keep", { desc = "Fugitive Save current buffer and git add" }, opts)
 )
 map(
   "n",
   "<leader>gmv",
   ":GMove",
-  vim.tbl_extend("keep", { desc = "Move current buffer with git" }, opts)
+  vim.tbl_extend("keep", { desc = "Fugitive Move current buffer with git" }, opts)
 )
 map(
   "n",
   "<leader>gdel",
   ":GDelete<Return>",
-  vim.tbl_extend("keep", { desc = "Delete current buffer with git rm" }, opts)
+  vim.tbl_extend("keep", { desc = "Fugitive Delete current buffer with git rm" }, opts)
 )
 map(
   "n",
@@ -167,7 +173,7 @@ map(
   ":GRemove<Return>",
   vim.tbl_extend(
     "keep",
-    { desc = "Git rm but leaves current buffer open" },
+    { desc = "Fugitive Git rm but leaves current buffer open" },
     opts
   )
 )
@@ -175,25 +181,25 @@ map(
   "n",
   "<leader>gd",
   ":Git diff<Return>",
-  vim.tbl_extend("keep", { desc = "Bring up the file diff side by side" }, opts)
+  vim.tbl_extend("keep", { desc = "Fugitive Bring up the file diff side by side" }, opts)
 )
 map(
   "n",
   "<leader>gl",
   ":G log --oneline<Return>",
-  vim.tbl_extend("keep", { desc = "Git log oneline" }, opts)
+  vim.tbl_extend("keep", { desc = "Fugitive Git log oneline" }, opts)
 )
 map(
   "n",
   "<leader>ga",
   ":Git add<Return>",
-  vim.tbl_extend("keep", { desc = "Add current file to git" }, opts)
+  vim.tbl_extend("keep", { desc = "Fugitive Add current file to git" }, opts)
 )
 map(
   "n",
   "<leader>gs",
   ":G<Return>",
-  vim.tbl_extend("keep", { desc = "Status of the git repository" }, opts)
+  vim.tbl_extend("keep", { desc = "Fugitive Status of the git repository" }, opts)
 )
 
 -- NvimTreeToggle
@@ -201,31 +207,7 @@ map(
   "n",
   "<F6>",
   ":NvimTreeToggle<Return>",
-  vim.tbl_extend("keep", { desc = "Toggle NvimTree" }, opts)
-)
-map("n", "<leader>tt", function()
-  require("base46").toggle_transparency()
-end, vim.tbl_extend("keep", { desc = "Toggle Transparency" }, opts))
-
--- Insert mode
-map(
-  { "i", "v" },
-  "jk",
-  "<ESC>",
-  vim.tbl_extend("keep", { desc = "Exit current mode" }, opts)
-)
-map(
-  "i",
-  "<C-o>",
-  "<esc>viwUea",
-  vim.tbl_extend("keep", { desc = "Change word to uppercase" }, opts)
-)
-
-map(
-  "t",
-  "<leader>jk",
-  "<C-\\><C-n>",
-  vim.tbl_extend("keep", { desc = "Exit terminal mode" }, opts)
+  vim.tbl_extend("keep", { desc = "NvimTree Toggle NvimTree" }, opts)
 )
 
 -- TMUX
@@ -233,55 +215,54 @@ map(
   "n",
   "<C-h>",
   "<cmd> TmuxNavigateLeft <cr>",
-  vim.tbl_extend("keep", { desc = "window left" }, opts)
+  vim.tbl_extend("keep", { desc = "TMUX Window left" }, opts)
 )
 map(
   "n",
   "<C-j>",
   "<cmd> TmuxNavigateDown <cr>",
-  vim.tbl_extend("keep", { desc = "window down" }, opts)
+  vim.tbl_extend("keep", { desc = "TMUX Window down" }, opts)
 )
 map(
   "n",
   "<C-k>",
   "<cmd> TmuxNavigateUp <cr>",
-  vim.tbl_extend("keep", { desc = "window up" }, opts)
+  vim.tbl_extend("keep", { desc = "TMUX Window up" }, opts)
 )
 map(
   "n",
   "<C-l>",
   "<cmd> TmuxNavigateRight <cr>",
-  vim.tbl_extend("keep", { desc = "window right" }, opts)
+  vim.tbl_extend("keep", { desc = "TMUX Window right" }, opts)
 )
 
--- Go debugger
+-- Debugging
 map(
   "n",
   "<leader>db",
   "<cmd> DapToggleBreakpoint <CR>",
-  vim.tbl_extend("keep", { desc = "Add breakpoint at line" }, opts)
+  vim.tbl_extend("keep", { desc = "Dap Add breakpoint at line" }, opts)
 )
 map(
   "n",
   "<leader>so",
   "<CMD> DapStepOver <CR>",
-  vim.tbl_extend("keep", { desc = "Step over the breakpoint" }, opts)
+  vim.tbl_extend("keep", { desc = "Dap Step over the breakpoint" }, opts)
 )
 map("n", "<leader>dui", function()
   local widgets = require "dap.ui.widgets"
   local sidebar = widgets.sidebar(widgets.scopes)
   sidebar.open()
-end, vim.tbl_extend("keep", { desc = "Open debugging ui" }, opts))
+end, vim.tbl_extend("keep", { desc = "Dap Open debugging ui" }, opts))
 
--- Debug nearest go test to cursor
 map("n", "<leader>dgt", function()
   require("dap-go").debug_test()
-end, vim.tbl_extend("keep", { desc = "Debug go test" }, opts))
+end, vim.tbl_extend("keep", { desc = "Go Debug Go test" }, opts))
 
 -- Rerun last test
 map("n", "<leader>dgl", function()
   require("dap-go").debug_last()
-end, vim.tbl_extend("keep", { desc = "Debug last go test" }, opts))
+end, vim.tbl_extend("keep", { desc = "Go Debug last go test" }, opts))
 
 map(
   "n",
@@ -291,11 +272,10 @@ map(
   end,
   vim.tbl_extend(
     "keep",
-    { desc = "Debug Python Test method under cursor" },
+    { desc = "Python Debug Python Test method under cursor" },
     opts
   )
 )
-
 map(
   "n",
   "<leader>dpc",
@@ -304,7 +284,7 @@ map(
   end,
   vim.tbl_extend(
     "keep",
-    { desc = "Debug Python Test class under cursor" },
+    { desc = "Python Debug Python Test class under cursor" },
     opts
   )
 )
@@ -314,19 +294,19 @@ map(
   "n",
   "<leader>gsj",
   "<cmd> GoTagAdd json <CR>",
-  vim.tbl_extend("keep", { desc = "Add json struct tags" }, opts)
+  vim.tbl_extend("keep", { desc = "Go Add JSON struct tags" }, opts)
 )
 map(
   "n",
   "<leader>gsy",
   "<cmd> GoTagAdd yaml <CR>",
-  vim.tbl_extend("keep", { desc = "Add yaml struct tags" }, opts)
+  vim.tbl_extend("keep", { desc = "Go Add YAML struct tags" }, opts)
 )
 map(
   "n",
   "<leader>gmt",
   "<cmd> GoMod tidy <CR>",
-  vim.tbl_extend("keep", { desc = "Run go mod tidy" }, opts)
+  vim.tbl_extend("keep", { desc = "Go Run go mod tidy" }, opts)
 )
 map(
   "n",
@@ -334,7 +314,7 @@ map(
   "<cmd> GoTestAdd <CR>",
   vim.tbl_extend(
     "keep",
-    { desc = "Add test for the func under the cursor" },
+    { desc = "Go Add test for the func under the cursor" },
     opts
   )
 )
@@ -342,7 +322,7 @@ map(
   "n",
   "<leader>gtall",
   "<cmd> GoTestsAll <CR>",
-  vim.tbl_extend("keep", { desc = "Add tests for all functions in file" }, opts)
+  vim.tbl_extend("keep", { desc = "Go Add tests for all functions in file" }, opts)
 )
 
 -- Todo
@@ -350,7 +330,7 @@ map(
   "n",
   "<leader>ft",
   "<cmd> TodoTelescope <CR>",
-  vim.tbl_extend("keep", { desc = "Open todo list in telescope" }, opts)
+  vim.tbl_extend("keep", { desc = "TODO Open todo list in telescope" }, opts)
 )
 
 -- Trouble
@@ -358,7 +338,7 @@ map(
   "n",
   "<leader>tr",
   "<cmd> TroubleToggle <CR>",
-  vim.tbl_extend("keep", { desc = "See all diagonstic troubles " }, opts)
+  vim.tbl_extend("keep", { desc = "Trouble See all diagonstic troubles " }, opts)
 )
 
 -- LSP Saga
@@ -366,49 +346,49 @@ map(
   "n",
   "<C-n>",
   "<CMD> Lspsaga diagnostic_jump_next <CR>",
-  vim.tbl_extend("keep", { desc = "Go to next error" }, opts)
+  vim.tbl_extend("keep", { desc = "LSPSaga Go to next error" }, opts)
 )
 map(
   "n",
   "K",
   "<CMD> Lspsaga hover_doc <CR>",
-  vim.tbl_extend("keep", { desc = "Get documentation" }, opts)
+  vim.tbl_extend("keep", { desc = "LSPSaga Get documentation" }, opts)
 )
 map(
   "n",
   "<leader>f",
   "<Cmd>Lspsaga finder<CR>",
-  vim.tbl_extend("keep", { desc = "Find identifier references" }, opts)
+  vim.tbl_extend("keep", { desc = "LSPSaga Find identifier references" }, opts)
 )
 map(
   "n",
   "ca",
   "<Cmd>Lspsaga code_action<CR>",
-  vim.tbl_extend("keep", { desc = "Show available code actions" }, opts)
+  vim.tbl_extend("keep", { desc = "LSPSaga Show available code actions" }, opts)
 )
 map(
   "n",
   "gd",
   "<Cmd>Lspsaga goto_definition<CR>",
-  vim.tbl_extend("keep", { desc = "Go to definition" }, opts)
+  vim.tbl_extend("keep", { desc = "LSPSaga Go to definition" }, opts)
 )
 map(
   "n",
   "<leader>pd",
   "<Cmd>Lspsaga peek_definition<CR>",
-  vim.tbl_extend("keep", { desc = "Peek definition" }, opts)
+  vim.tbl_extend("keep", { desc = "LSPSaga Peek definition" }, opts)
 )
 map(
   "n",
-  "gr",
+  "<leader>rn",
   "<Cmd>Lspsaga rename<CR>",
-  vim.tbl_extend("keep", { desc = "Rename identifier" }, opts)
+  vim.tbl_extend("keep", { desc = "LSPSaga Rename identifier" }, opts)
 )
 
 -- Crates
 map("n", "<leader>rcu", function()
   require("crates").upgrade_all_crates()
-end, vim.tbl_extend("keep", { desc = "update crates" }, opts))
+end, vim.tbl_extend("keep", { desc = "Crates Update crates" }, opts))
 
 -- Obsession
 map(
@@ -417,7 +397,7 @@ map(
   "<CMD>Obsession .<CR>",
   vim.tbl_extend(
     "keep",
-    { desc = "Save session file in current directory" },
+    { desc = "Obsession Save session file in current directory" },
     opts
   )
 )
@@ -427,5 +407,5 @@ map(
   "v",
   "<leader>sc",
   ":Silicon <CR> ",
-  vim.tbl_extend("keep", { desc = "Screenshot Code" }, opts)
+  vim.tbl_extend("keep", { desc = "NVIM-Silicon Screenshot Code" }, opts)
 )
