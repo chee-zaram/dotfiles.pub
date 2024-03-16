e4bf91e1c6f019623a202d8316572b4e19e4853c
Emmanuel Chee-zaram Okeke <cheezaram.okeke@gmail.com>
2024-03-16 20:29:47 +0100

    add wezterm config

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,26 @@
+local ok, wezterm = pcall(require, 'wezterm')
+if not ok then return end
+
+return {
+  -- color_scheme = 'Catppuccin Mocha',
+  color_scheme = 'Oxocarbon Dark (Gogh)',
+  enable_tab_bar = false,
+  font_size = 13.0,
+  window_background_opacity = 1.0,
+  window_decorations = 'RESIZE',
+  keys = {
+    {
+      key = 'f',
+      mods = 'CTRL',
+      action = wezterm.action.ToggleFullScreen,
+    },
+  },
+  mouse_bindings = {
+    -- Ctrl-click will open the link under the mouse cursor
+    {
+      event = { Up = { streak = 1, button = 'Left' } },
+      mods = 'CTRL',
+      action = wezterm.action.OpenLinkAtMouseCursor,
+    },
+  },
+}
