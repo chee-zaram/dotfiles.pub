d5a8d7f1d47618a15da63b46d14afd6fdd02952b
Emmanuel Chee-zaram Okeke <cheezaram.okeke@gmail.com>
2024-03-24 19:43:27 +0100

    change theme to tokyonight

--- .config/nvim/lua/chadrc.lua
+++ .config/nvim/lua/chadrc.lua
@@ -1,12 +1,14 @@
 local M = {}
 
 M.ui = {
-  theme = "oxocarbon",
+  theme = "tokyonight",
 
   hl_override = {
     ["@comment"] = { italic = true },
     ["@keyword.conditional"] = { italic = true },
     ["@keyword.conditional.ternary"] = { italic = true },
+
+    Include = { italic = true, },
   },
 }
 
