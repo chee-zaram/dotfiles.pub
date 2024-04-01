5f94403c87e549fb2623176226cb63c401a4f5db
Emmanuel Chee-zaram Okeke <cheezaram.okeke@gmail.com>
2024-04-01 16:17:03 +0100

    add harpoon config

--- .config/nvim/lua/plugins/harpoon.lua
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -3,5 +3,37 @@ return {
   "ThePrimeagen/harpoon",
   event = "VeryLazy",
   branch = "harpoon2",
-  opts = {},
+  config = function(_, _)
+    require "configs.my-harpoon"
+  end,
+  keys = function()
+    local keys = {
+      {
+        "<leader>a",
+        function()
+          require("harpoon"):list():append()
+        end,
+        desc = "Harpoon Add File",
+      },
+      {
+        "<leader>h",
+        function()
+          local harpoon = require("harpoon")
+          harpoon.ui:toggle_quick_menu(harpoon:list())
+        end,
+        desc = "Harpoon Quick Menu",
+      }
+    }
+
+    for i = 1, 5 do
+      table.insert(keys, {
+        "<leader>" .. i,
+        function()
+          require("harpoon"):list():select(i)
+        end,
+        desc = "Harpoon Goto File " .. i,
+      })
+    end
+    return keys
+  end
 }
